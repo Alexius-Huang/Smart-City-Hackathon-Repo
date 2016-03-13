44658b65478d491cda7680064ec0ef6ffa772129
freedomsean <t101598009@ntut.org.tw>
2016-03-13 10:55:14 +0800

    add diagram module

--- app/assets/javascripts/diagram.js
+++ app/assets/javascripts/diagram.js
@@ -1,25 +1,44 @@
 (function (window) {
 
+
 	function exampleData() {
 		  return  [
 		      { 
-		        "label": "博士",
+		        "label": "反對",
+		        "value" : 64
+		      } , 
+		      { 
+		        "label": "贊成",
+		        "value" : 36
+		      } 
+		    ];
+		}
+
+		function exampleData2() {
+		  return  [
+		      { 
+		        "label": "> 300",
 		        "value" : 29.76
 		      } , 
 		      { 
-		        "label": "碩士",
+		        "label": "100 - 300",
 		        "value" : 30
 		      } , 
 		      { 
-		        "label": "學士",
+		        "label": "70 - 100",
 		        "value" : 32.807
 		      } , 
 		      { 
-		        "label": "高中職",
+		        "label": "40 - 70",
+		        "value" : 196.459
+		      } , 
+		      { 
+		        "label": "< 40",
 		        "value" : 196.459
 		      } 
 		    ];
 		}
+
 
 	window.drawPieChart = function(domStr, data) {
 		nv.addGraph(function() {
@@ -29,14 +48,21 @@
       			.showLabels(true)     //Display pie labels
       			.labelType("percent"); //Configure what type of data to show in the label. Can be "key", "value" or "percent"
 
+
 		    d3.select(domStr)
-		        .datum(exampleData())
+		        .datum(data)
 		        .transition().duration(350)
 		        .call(chart);
 
 		  return chart;
 		});
 
+	}
+
+
+	window.drawAllCart = function() {
+		drawPieChart('.static_contaier svg',  exampleData());
+        drawPieChart('.static_contaier2 svg', exampleData2());
 	}
 
 })(window)
